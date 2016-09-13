31cbc9a79ee467a435c54d3dd26fbf571830c7f6
Aleksander Ciesiołkiewicz <aleksander.ciesiolkiewicz@sdimedia.com>
2016-09-13 14:42:26 +0200

    fix controls component

--- video-player/src/components/controls-component.ts
+++ video-player/src/components/controls-component.ts
@@ -1,14 +1,14 @@
 import {component} from 'jodi-ui-components';
-import {el, text, s, d} from 'jodi-ui-dom';
+import {el, text, d} from 'jodi-ui-dom';
 import {PlaybackActions} from '../actions/playback-actions';
 import {AppState} from '../states/app-state';
 
 export function controlsComponent(state: AppState, actions: PlaybackActions) {
     const render = () => {
         if (state.playing) {
-            el('button', s({'class:': 'pause'}), d({onclick: actions.pause}), () => text('Pause'));
+            el('button', d({'class:': 'pause', onclick: actions.pause}), () => text('Pause'));
         } else {
-            el('button', s({'class': 'play'}), d({onclick: actions.play}), () => text('Play'));
+            el('button', d({'class': 'play', onclick: actions.play}), () => text('Play'));
         }
     };
 
